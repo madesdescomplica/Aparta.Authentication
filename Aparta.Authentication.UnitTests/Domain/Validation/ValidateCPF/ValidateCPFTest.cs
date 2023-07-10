03ba46b4f1961335890f3a1aed785ae6f769f122
Mades Eladrian <mades.eladrian@gmail.com>
2023-07-09 22:48:06 -0300

    ensure ValidateCPF returns false if receive cpf number with incorrect number

--- Aparta.Authentication.UnitTests/Domain/Validation/ValidateCPF/ValidateCPFTest.cs
+++ Aparta.Authentication.UnitTests/Domain/Validation/ValidateCPF/ValidateCPFTest.cs
@@ -19,4 +19,30 @@ public class ValidateCPFTest
 
         isValid.Should().BeTrue();
     }
+
+    [Theory(DisplayName = nameof(Should_Return_False_If_Receive_Incorrect_CPF_Number))]
+    [Trait("Domain", "ValidateCPF - Validation")]
+    [MemberData(
+        nameof(ValidateCPFTestDataGenerator.GetInvalidsCPFsNumbers),
+        parameters: 10,
+        MemberType = typeof(ValidateCPFTestDataGenerator)
+    )]
+    public void Should_Return_False_If_Receive_Incorrect_CPF_Number(string cpf)
+    {
+        var isValid = DomainValidation.ValidateCPF.IsValid(cpf);
+
+        isValid.Should().BeFalse();
+    }
+
+    [Theory(DisplayName = nameof(Should_Return_False_If_Receive_Incorrect_CPF_Number))]
+    [Trait("Domain", "ValidateCPF - Validation")]
+    [InlineData("00000000000")]
+    [InlineData("11111111111")]
+    [InlineData("22222222222")]
+    public void Should_Return_False_If_Receive_Incorrect_CPF_Number_With_Repeat_Numbers(string cpf)
+    {
+        var isValid = DomainValidation.ValidateCPF.IsValid(cpf);
+
+        isValid.Should().BeFalse();
+    }
 }

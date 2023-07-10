9ece6665b13f9da9fd1b74c47043d15b08328ed4
Mades Eladrian <mades.eladrian@gmail.com>
2023-07-10 09:07:32 -0300

    ensure UnitOfWork calls Commit with correct values

---
+++ Aparta.Authentication.Infra.Data.EF/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Aparta.Authentication.Application.Interfaces;
+
+namespace Aparta.Authentication.Infra.Data.EF;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApartaAccountDbContext _context;
+
+    public UnitOfWork(ApartaAccountDbContext context)
+        => _context = context;
+
+    public Task Commit(CancellationToken cancellationToken)
+        => _context.SaveChangesAsync(cancellationToken);
+}
---
+++ Aparta.Authentication.IntegrationTests/Infra.Data.EF/UnitOfWork/UnitOfWorkTest.cs
@@ -0,0 +1,35 @@
+﻿using UnitOfWorkInfra = Aparta.Authentication.Infra.Data.EF;
+
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Aparta.Authentication.IntegrationTests.Infra.Data.EF.UnitOfWork;
+
+[Collection(nameof(UnitOfWorkTestFixture))]
+public class UnitOfWorkTest
+{
+    private readonly UnitOfWorkTestFixture _fixture;
+
+    public UnitOfWorkTest(UnitOfWorkTestFixture testFixture)
+        => _fixture = testFixture;
+
+    [Fact(DisplayName = nameof(Should_Call_Commit_With_Correct_Values))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task Should_Call_Commit_With_Correct_Values()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var exampleAccountsList = _fixture.GetExampleAccountsList();
+        await dbContext.AddRangeAsync(exampleAccountsList);
+        var unitOfWork = new UnitOfWorkInfra.UnitOfWork(dbContext);
+
+        await unitOfWork.Commit(CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var savedAccounts = assertDbContext.Accounts
+            .AsNoTracking()
+            .ToList();
+
+        savedAccounts.Should().HaveCount(exampleAccountsList.Count);
+    }
+}
